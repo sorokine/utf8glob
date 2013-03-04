c7365b84c30c517bb75f0cdd74135440d50e1b7a
Alex Sorokine <sorokine@gmail.com>
2013-03-04 13:50:00 -0500

    now problem with й

--- src/main/java/info/sorokine/utf8glob/App.java
+++ src/main/java/info/sorokine/utf8glob/App.java
@@ -1,10 +1,11 @@
 package info.sorokine.utf8glob;
 
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.PathMatcher;
 import java.nio.file.Paths;
 
@@ -20,24 +21,27 @@ public class App
 			try {
 				Files.createFile(Paths.get("abc.html"));
 				Files.createFile(Paths.get("эюя.html")); // UTF-8 string
+				Files.createFile(Paths.get("ой-ёй-ЁЙ.html")); // UTF-8 string with composite characters
 			} catch (FileAlreadyExistsException e) {}
 
 			match("glob:*.html");
 			match("glob:a*.html");
 			match("glob:э*.html");
+			match("glob:о*.html");
+			match("glob:*Й.html");
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
     }
     
-    public static void match(String glob) {
+    public static void match(String glob) throws IOException {
     	System.err.println("Glob: " + glob);
         PathMatcher pmatcher = FileSystems.getDefault().getPathMatcher(glob);
-        File dir = new File(".");
-        for (File file : dir.listFiles()) 
-			if (pmatcher.matches(file.toPath().getFileName()))
-				System.err.println("" + file);
+        DirectoryStream<Path> dir = Files.newDirectoryStream(Paths.get("."));
+        for (Path file : dir) 
+			if (pmatcher.matches(file.getFileName())) 
+				System.err.println("" + file + " canRead? " + file.toFile().canRead());
     	System.err.println();
 	}
 }
